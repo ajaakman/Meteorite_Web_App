03230dc4ff777b4ece3589758e58cbc8984d2df7
arturjaakman <artur.jaakman@gmail.com>
2018-12-15 13:12:53 +0000

    Added Bar Chart

---
+++ app/assets/javascripts/barchart.js
@@ -0,0 +1,82 @@
+function initBarChart(meteoriteData){
+  Highcharts.chart('barChartContainer', {
+      chart: {
+          type: 'bar',
+          zoomType: 'x',
+          panning: true,
+          panKey: 'shift',
+        scrollablePlotArea: {
+            minWidth: 500,
+            scrollPositionX: 1
+        }
+
+      },
+      title: {
+          text: 'Meteorite Data'
+      },
+      xAxis: {
+          categories: meteoriteData[0],
+          title: {
+              text: null
+          }
+      },
+      yAxis: [{
+            title: {
+                text: 'Fragments Sold',
+                align: 'left',
+                y: -15,
+                reserveSpace: false
+            }
+        }, {
+            title: {
+                text: 'Total Weight',
+                align: 'left',
+                y: -15,
+                reserveSpace: false
+            },
+        }, {
+            title: {
+                text: 'Total Value',
+                align: 'left',
+                y: -15,
+                reserveSpace: false
+            },
+        }],
+      tooltip: {
+          valueSuffix: ''
+      },
+      plotOptions: {
+          bar: {
+              dataLabels: {
+                  enabled: true
+              }
+          }
+      },
+      legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+          shadow: true
+      },
+      credits: {
+          enabled: false
+      },
+      series: [{
+          name: 'Total Fragments Sold',
+          data: meteoriteData[1]
+      }, {
+          name: 'Total Weight Sold',
+          data: meteoriteData[2],
+          yAxis: 1
+      }, {
+          name: 'Total Cost',
+          data: meteoriteData[3],
+          yAxis: 2
+      }]
+  });
+}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,28 @@
 class HomeController < ApplicationController
   def home
+
+    @meteoritesData = Array.new
+    @meteoriteName = Array.new
+    @meteoriteFragmentsSold = Array.new
+    @meteoriteWeightSold = Array.new
+    @meteoriteValue = Array.new
+    @orders = Order.all
+
+    @orders.each do |order|
+      if order.completed == true
+        if @meteoriteName.index(order.posting.meteorite.name) == nil
+          @meteoriteName << order.posting.meteorite.name
+          @meteoriteFragmentsSold << 1
+          @meteoriteWeightSold << order.posting.weight
+          @meteoriteValue << order.posting.cost
+        else
+          @meteoriteFragmentsSold[@meteoriteName.index(order.posting.meteorite.name)] += 1
+          @meteoriteWeightSold[@meteoriteName.index(order.posting.meteorite.name)] += order.posting.weight
+          @meteoriteValue[@meteoriteName.index(order.posting.meteorite.name)] += order.posting.cost
+        end
+      end
+    end
+    @meteoritesData << @meteoriteName << @meteoriteFragmentsSold << @meteoriteWeightSold << @meteoriteValue
   end
 
   def about
